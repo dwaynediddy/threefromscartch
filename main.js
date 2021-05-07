3e1c08e7be9d558cd3929ccb5cfa9880162f1e9c
dwaynediddy <burrows.dwayne@icloud.com>
2021-05-07 14:01:47 +0200

    added plane

--- main.js
+++ main.js
@@ -8,10 +8,53 @@ const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 10
 
 const render = new THREE.WebGLRenderer()
 
-console.log(scene)
-console.log(camera)
-console.log(render)
-
 render.setSize(innerWidth, innerHeight)
 document.body.appendChild(render.domElement)
 
+// const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+
+// const material = new THREE.MeshBasicMaterial({ color: 0x00FF00})
+
+// const mesh = new THREE.Mesh(boxGeometry, material)
+// console.log(mesh)
+
+// scene.add(mesh)
+
+camera.position.z = 5
+
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10)
+
+const planeMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000,
+side: THREE.DoubleSide, flatShading: THREE.FlatShading })
+
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
+scene.add(planeMesh)
+
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(0, 0, 1)
+scene.add(light)
+
+console.log(planeGeometry)
+console.log(planeMesh.geometry.attributes.position.array)
+
+const { array } = planeMesh.geometry.attributes.position
+for (let i = 0; i < array.length; i += 3) {
+  const x = array[i]
+  const y = array[i + 1]
+  const z = array[i + 2]
+
+  array[i + 2] = z + Math.random()
+
+ console.log(array[i])
+}
+
+
+function animate() {
+  requestAnimationFrame(animate)
+  render.render(scene, camera)
+  // mesh.rotation.x += 0.01
+  // mesh.rotation.y += 0.01
+  // planeMesh.rotation.x += 0.01
+}
+
+animate()
