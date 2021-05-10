e61030dae38b3459806d6ecc8d3932ed73efb6e0
dwaynediddy <burrows.dwayne@icloud.com>
2021-05-10 19:39:29 +0200

    finishing gui and optimising code

--- main.js
+++ main.js
@@ -11,28 +11,30 @@ const world = {
   plane: {
     width: 10,
     height: 10,
+    widthSegments: 10,
+    heightSegments: 10,
   }
 }
 gui.add(world.plane, 'width', 1, 20).
-onChange(() => {
-  planeMesh.geometry.dispose()
-  planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, 10, 10)
-  const { array } = planeMesh.geometry.attributes.position
-for (let i = 0; i < array.length; i += 3) {
-  const x = array[i]
-  const y = array[i + 1]
-  const z = array[i + 2]
-
-  array[i + 2] = z + Math.random()
-
- console.log(array[i])
-}
-})
+onChange(generatePlane)
 
 gui.add(world.plane, 'height', 1, 20).
-onChange(() => {
+onChange(generatePlane)
+
+gui.add(world.plane, 'widthSegments', 1, 20).
+onChange(generatePlane)
+
+gui.add(world.plane, 'heightSegments', 1, 20).
+onChange(generatePlane)
+
+function generatePlane() {
   planeMesh.geometry.dispose()
-  planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, 10, 10)
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height, 
+    world.plane.widthSegments, 
+    world.plane.heightSegments
+  )
   const { array } = planeMesh.geometry.attributes.position
 for (let i = 0; i < array.length; i += 3) {
   const x = array[i]
@@ -42,8 +44,8 @@ for (let i = 0; i < array.length; i += 3) {
   array[i + 2] = z + Math.random()
 
  console.log(array[i])
+  }
 }
-})
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
